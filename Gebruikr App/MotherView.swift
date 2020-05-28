4c3c0a27e07a380c65698d88fdc865b33cbee536
Sander Paping <sander.paping@student.nhlstenden.com>
2020-05-28 22:24:30 +0200

    Fix NavigationView And Onboarding only when first open app

---
+++ Gebruikr App/MotherView.swift
@@ -0,0 +1,21 @@
+//
+//  MotherView.swift
+//  Gebruikr App
+//
+//  Created by Sander Paping on 28/05/2020.
+//  Copyright © 2020 Sander Paping. All rights reserved.
+//
+
+import SwiftUI
+
+struct MotherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MotherView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotherView()
+    }
+}
